52c96e65aaddc21c6d85a6b5215c9cdd5671d51b
valkim55 <vesper5521@gmail.com>
2022-09-06 14:46:24 -0400

    get uv index as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
 
 // API key for using openweather source
-var apiKey = "b711e217ca46753654634ae778a24382";
-// declare a variable to store the user's city name input
+var apiKey = "98fbab5c25594a7fcd2eb52c792c6718";
+//var apiSecondKey = "b711e217ca46753654634ae778a24382";
+
+// declare variables to store the user's city name input, geo coordinates in a raw format and geo coordinates rounded to two decimals
 var cityName;
+var searchTerm;
+var allWeather;
+var lonRaw;
+var latRaw;
+var lon;
+var lat;
 
 // create a variable to store a reference to the <form> with an id city-form
 var cityFormEl = document.querySelector("#city-form");
@@ -29,40 +37,14 @@ var fiveDaysAfterContainer = document.querySelector("#day5");
 // everything before ? is the base URL for calling the Current Weather Data API
 // ? marks the boundary between the base URL of the API call and the query terms of the API call
 // q= is the query parameter where you can add any user input to specify the data we want to request in the API call. the value assigned to this parameter is called the query string
-// ampersand & indicates that we're adding another parameter after the query parameter, appid= concatenates another parameter where we'll add the key
-//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
-
-var getCityName = function(cityName) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units=imperial";
-    // call the fetch api to pass the query URL as a parameter
-    fetch(queryURL).then(function(response) {
-
-        // check if the response is ok - meaning the requested city name exists
-        if (response.ok) {
-            response.json().then(function(data) {
-                //after displayWeather function was created we will send the response data from getCityName to displayWeather
-                displayWeather(data, cityName);
-            });
-        } else {
-            alert("Error: City Name Not Found");
-        }
-    })
-
-    //handle the response in case there's a network error
-    .catch(function(error) {
-        alert("Unable to connect to server");
-    });
-};
-//getCityName("Seattle");
+// ampersand & indicates that we're adding another parameter after the query parameter, appid= concatenates another parameter where we'll add the key, units imperial means the measure unit (Farenheit, MPH etc.)
 
 
-
-//create a function to be executed when the form is submitted
 var formSubmitHandler = function(event) {
     event.preventDefault();
-    console.log(event);
+    //console.log(event);
 
-    // capture the vale from <input>
+    // capture the value from <input>
     var userCityName = cityInputEl.value.trim();
     // check if the input is an actual city name
     if (userCityName) {
@@ -76,21 +58,43 @@ var formSubmitHandler = function(event) {
 };
 
 
-//create a functin to display info about the city, for now the array of data for that city and the serchTerm we'll set up
-var displayWeather = function(cityInfo, searchTerm) {
-    console.log(cityInfo);
-    console.log(searchTerm);
+// create a function 
+var getCityName = function(cityName) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&units=imperial";
+    // call the fetch api to pass the query URL as a parameter
+    fetch(queryURL).then(function(response) {
+
+        // check if the response is ok - meaning the requested city name exists
+        if (response.ok) {
+            response.json().then(function(data) {
+                
+                
+                //send response data to getCityCoordinates function
+                getCityCoordinates(data, cityName);
+
+            });
+        } else {
+            alert("Error: City Name Not Found");
+        }
+    })
+
+    //handle the response in case there's a network error
+    .catch(function(error) {
+        alert("Unable to connect to server");
+    });
+};
+
+
+
+var getCityCoordinates = function(cityInfo, searchTerm) {
+    lonRaw = cityInfo.coord.lon;
+    latRaw = cityInfo.coord.lat;
+    lon = Math.round(lonRaw * 100)/100;
+    lat = Math.round(latRaw * 100)/100; 
 
     // make sure to clear out old content before displaying new input content
-    cityInfoContainerEl.textContent = "";
     citySearchTerm.textContent = "";
-    oneDayAfterContainer.textContent = "";
-    twoDaysAfterContainer.textContent = "";
-    threeDaysAfterContainer.textContent = "";
-    fourDaysAfterContainer.textContent = "";
-    fiveDaysAfterContainer.textContent = "";
 
-   
     // dynamically create a span element to hold a currently viewed city name and append to search-term div
     var currentCity = document.createElement("span");
     currentCity.classList = "text-uppercase";
@@ -104,6 +108,18 @@ var displayWeather = function(cityInfo, searchTerm) {
     citySearchTerm.appendChild(currentDateEl);
 
 
+    console.log(searchTerm, lon, lat);
+
+    getSearchHistory(searchTerm);
+    console.log(searchTerm);
+    getAllWeather(allWeather);
+};
+
+
+
+//create a function to dynamically generate element for search history
+var getSearchHistory = function(searchTerm) {
+    
     //dynamically create buttons/holders to store the city name in the search history container
     var cityHistoryList = document.createElement("button");
     cityHistoryList.classList = "search-history-container text-uppercase";
@@ -111,12 +127,50 @@ var displayWeather = function(cityInfo, searchTerm) {
     cityHistoryEl.textContent = searchTerm;
     cityHistoryList.appendChild(cityHistoryEl);
     searchHistoryContainer.appendChild(cityHistoryList);
+};
 
+
+
+
+
+//create a function to get current UV index using lon and lat
+var getAllWeather = function(allWeather) {
+    var uvQueryUrl = "https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&units=imperial&exclude=minutely,hourly&appid="+apiKey;
+    fetch(uvQueryUrl).then(function(response) {
+        //check if the response is ok
+        if(response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
+                
+                displayWeather(data);
+                console.log(data);
+            });
+        } else {
+            console.log("data not found");
+        }
+    })
+    .catch(function(error) {
+        console.log("unable to connect to server");
+    });
+};
+
+
+var displayWeather = function(cityInfo) {
+    //console.log(cityInfo);
+
+    // make sure to clear out old content before displaying new input content
+    cityInfoContainerEl.textContent = "";
+    
 
     // go throught the data for selected city and format the data for temperature, wind, humidity and UV
-    var temp = "Temp: " + cityInfo.main.temp + "°F";
-    var wind = "Wind: " + cityInfo.wind.speed + " MPH";
-    var humidity = "Humidity: " + cityInfo.main.humidity + "%";
+    var temp = "Temp: " + cityInfo.current.temp + "°F";
+    console.log(temp);
+    var wind = "Wind: " + cityInfo.current.wind_speed + " MPH";
+    console.log(wind);
+    var humidity = "Humidity: " + cityInfo.current.humidity + "%";
+    console.log(humidity);
+    var currentUV = "UV index: " + cityInfo.current.uvi;
+    console.log(currentUV);
     
     //create a container and span for each data. append span to container, and then append that container to cityInfo-container div
     var tempEl = document.createElement("div");
@@ -140,6 +194,13 @@ var displayWeather = function(cityInfo, searchTerm) {
     humidEl.appendChild(humidTitleEl);
     cityInfoContainerEl.appendChild(humidEl);
 
+    var uvIndexEl = document.createElement("div");
+    uvIndexEl.classList = "param-list-item flex-row justify-space-between align-center";
+    var uvIndexTitleEl = document.createElement("span");
+    uvIndexTitleEl.textContent = currentUV;
+    uvIndexEl.appendChild(uvIndexTitleEl);
+    cityInfoContainerEl.appendChild(uvIndexEl);
+
     // call the function for the next five days to display after submitting city name
     nextFiveDates();
     
@@ -148,6 +209,14 @@ var displayWeather = function(cityInfo, searchTerm) {
 
 // create function to generate the dates for the next five days from current view date
 var nextFiveDates = function() {
+
+    // make sure to clear out old content before displaying new input content
+    oneDayAfterContainer.textContent = "";
+    twoDaysAfterContainer.textContent = "";
+    threeDaysAfterContainer.textContent = "";
+    fourDaysAfterContainer.textContent = "";
+    fiveDaysAfterContainer.textContent = "";
+
     // get the dates for the next 5 days cards
     var dayOneAfter = moment().add(1, 'd').format("L");
     var oneDayAfterEl = document.createElement("h4");
@@ -181,11 +250,5 @@ var nextFiveDates = function() {
 };
 
 
-
-
-
 // add submit event listener to the cityFormEl
 cityFormEl.addEventListener("submit", formSubmitHandler);
-
-
-
